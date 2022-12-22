93e0063dedce1e0fcffea40b2eb9e87c7f949404
김영민 <kym@gim-yeongmin-ui-MacBookPro.local>
2022-12-22 23:09:46 +0900

    update:placed the declaration above

--- idnd_platform_contract/Product.sol
+++ idnd_platform_contract/Product.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.5.4;
 import "./Storage.sol";
 
 contract Product is Storage {
-    
+
     event EditProduct(string productUuid, string name, uint256 price);
     event EditProductStatusPause(string productUuid);
     event EditProductStatusByOwner(string productUuid, string hashValue, Type.ProductStatus status);
-    
+
     function addProduct(string  calldata _productUuid,
                         string  calldata _name,
                         string  calldata _ipfsLink,
                         string  calldata _encryptedIpfsKey,
                         uint256 _price) external {
-        
+
         require(customers[msg.sender].status != Type.CustomerStatus.NONMEMBER, "You must regist customer first");
         require(creators[msg.sender].tier != Type.CreatorTier.NONE, "You must regist creator first");
         require(products[_productUuid].status == Type.ProductStatus.NONE, "Product already exist");
@@ -22,11 +22,11 @@ contract Product is Storage {
 
         products[_productUuid] = Type.ProductInfo(msg.sender, _name, _ipfsLink, _encryptedIpfsKey, "", _price, 0, 0, 0, Type.ProductStatus.REQUEST_SALE);
     }
-    
+
     function editProduct(string calldata _productUuid,
                          string calldata _name,
                          uint256         _price) external {
-        
+
         Type.ProductInfo storage p = products[_productUuid];
         require(p.status  != Type.ProductStatus.NONE, "Product does not exist");
         require(msg.sender == p.creator, "You are not the creator of this product");
@@ -37,12 +37,12 @@ contract Product is Storage {
         p.price     = _price;
         p.hashValue = "";
         p.status    = Type.ProductStatus.REQUEST_UPDATE;
-        
+
         emit EditProduct(_productUuid, _name, _price);
     }
-    
+
     function editProductStatusPause(string calldata _productUuid) external {
-        
+
         Type.ProductInfo storage p = products[_productUuid];
         require(p.status  != Type.ProductStatus.NONE, "Product does not exist");
         require(msg.sender == p.creator, "You are not the creator of this product");
@@ -51,12 +51,12 @@ contract Product is Storage {
 
         emit EditProductStatusPause(_productUuid);
     }
-    
+
     function editProductStatusByOwner(string calldata _productUuid, string calldata _hashValue, Type.ProductStatus _status) external {
-        
-        require(msg.sender == owner, "Not owner");
-        
+
         Type.ProductInfo storage p = products[_productUuid];
+
+        require(msg.sender == owner, "Not owner");
         require(p.status  != Type.ProductStatus.NONE, "Product does not exist");
 
         p.hashValue = _hashValue;
@@ -66,10 +66,10 @@ contract Product is Storage {
     }
 
     function removeProduct(string calldata _productUuid) external {
-        
+
         require(products[_productUuid].status != Type.ProductStatus.NONE, "Product does not exist");
         require(msg.sender == products[_productUuid].creator, "Only product registrants can remove");
-        
+
         delete products[_productUuid];
     }
 }
--- idnd_platform_contract/Review.sol
+++ idnd_platform_contract/Review.sol
@@ -8,9 +8,10 @@ contract Review is Storage {
                         uint8  _rating) external {
 
         Type.CustomerInfo storage c = customers[msg.sender];
+        Type.ProductInfo  storage p = products[_productUuid];
+
         require(c.status != Type.CustomerStatus.NONMEMBER, "You must regist customer first");
         require(_rating <= 10, "Rating must under 10");
-        Type.ProductInfo storage p = products[_productUuid];
         require(p.status != Type.ProductStatus.NONE, "Product does not exist");
         require(purchaseTable[msg.sender][_productUuid].status == Type.PurchaseStatus.ACCEPT, "This product has not been purchased");
         require(reviews[msg.sender][_productUuid].status == Type.ReviewStatus.NONE, "Review already exist");
@@ -24,12 +25,12 @@ contract Review is Storage {
     function removeReview(string calldata _productUuid) external {
 
         Type.CustomerInfo storage c = customers[msg.sender];
-        Type.ReviewInfo storage r = reviews[msg.sender][_productUuid];
+        Type.ReviewInfo   storage r = reviews[msg.sender][_productUuid];
+        Type.ProductInfo  storage p = products[_productUuid];
 
         require(c.status != Type.CustomerStatus.NONMEMBER, "You must regist customer first");
         require(r.status != Type.ReviewStatus.NONE, "Review does not exist");
 
-        Type.ProductInfo storage p = products[_productUuid];
 
         p.totalReviewRating -= r.rating;
         p.reviewCount--;
@@ -40,17 +41,17 @@ contract Review is Storage {
 
     function editReview(string calldata _productUuid, uint8 _rating) external {
 
-    Type.CustomerInfo storage c = customers[msg.sender];
-    Type.ReviewInfo   storage r = reviews[msg.sender][_productUuid];
-    Type.ProductInfo  storage p = products[_productUuid];
+        Type.CustomerInfo storage c = customers[msg.sender];
+        Type.ReviewInfo   storage r = reviews[msg.sender][_productUuid];
+        Type.ProductInfo  storage p = products[_productUuid];
 
-    require(c.status != Type.CustomerStatus.NONMEMBER, "You must regist customer first");
-    require(_rating <= 10, "Rating must under 10");
-    require(purchaseTable[msg.sender][_productUuid].status == Type.PurchaseStatus.ACCEPT, "This product has not been purchased");
-    require(r.status == Type.ReviewStatus.ON, "Review is not available");
+        require(c.status != Type.CustomerStatus.NONMEMBER, "You must regist customer first");
+        require(_rating <= 10, "Rating must under 10");
+        require(purchaseTable[msg.sender][_productUuid].status == Type.PurchaseStatus.ACCEPT, "This product has not been purchased");
+        require(r.status == Type.ReviewStatus.ON, "Review is not available");
 
-    p.totalReviewRating -= r.rating;
-    p.totalReviewRating += _rating;
-    reviews[msg.sender][_productUuid] = Type.ReviewInfo(_rating, Type.ReviewStatus.ON);
+        p.totalReviewRating -= r.rating;
+        p.totalReviewRating += _rating;
+        reviews[msg.sender][_productUuid] = Type.ReviewInfo(_rating, Type.ReviewStatus.ON);
     }
 }
